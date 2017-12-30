6fe7d6330508b1e5e5c2821fbf0ce87d2f105256
Daniel Quintana <daniel.quintana@trivago.com>
2017-12-30 12:12:06 +0100

    Initial graphql configuration: express-graphql and graphql modules. Middleware to use graphqlHTTP in the path '/graphql'

---
+++ src/types/event.js
@@ -0,0 +1,23 @@
+const {
+    GraphQLObjectType,
+    GraphQLNonNull,
+    GraphQLID,
+    GraphQLString
+} = require('graphql');
+
+module.exports = new GraphQLObjectType({
+    name: 'Event',
+    fields: () => ({
+        id: {
+            type: new GraphQLNonNull(GraphQLID)
+        },
+        name: {
+            type: GraphQLString
+        },
+        date: {
+            type: GraphQLString
+        }
+    })
+
+
+})
